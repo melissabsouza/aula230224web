ee586311f7345fb2d192590058830e82647e492a
Mel <melissabsouzaa@gmail.com>
2024-02-23 12:04:11 -0300

    first commit

---
+++ aula230224/src/components/divtest/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from '../../assets/react.svg'
+
+function Divtest() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+  )
+}
+
+export default Divtest
